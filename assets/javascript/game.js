04e79ca4d8b7b5403a625f42dc8f6bd229ba0cc5
KimblesAndBits <wearethekimbles@gmail.com>
2019-05-01 16:46:51 -0700

    some working js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,47 +1,48 @@
-var info = ["name", "hp", "attack stat", "counter attack stat", "character image"];
+var characterChoices = [
+["Luke", 175, 4, 25, "assets/images/luke.jpeg"],
+["Han", 100, 8, 12, "assets/images/han.jpeg"],
+["Vader", 140, 6, 5, "assets/images/vader.jpeg"],
+["Boba-Fett", 125, 7, 16, "assets/images/fett.jpeg"]];
 
-var Luke = ["Luke", 175, 4, 25, "assets/images/luke.jpeg"];
-
-var Han = ["Han", 100, 8, 12, "assets/images/han.jpeg"];
-
-var Vader = ["Vader", 140, 6, 5, "assets/images/vader.jpeg"];
-
-var bobaFett = ["Boba Fett", 125, 7, 16, "assets/images/fett.jpeg"];
-
-var playerCharacter = {
+var character = {
     name: "?",
     hp: 0,
     attackStat: 0,
+    counterAttackStat: 0,
     characterImage: "?",
-    pick = function(arr) {
+    pickMe: function(arr) {
         this.name = arr[0];
         this.hp = arr[1];
         this.attackStat = arr[2];
+        this.counterAttackStat = arr[3]
         this.characterImage = arr[4];
     },
-    resetCharacter = function() {
+    resetCharacter: function() {
         this.name = "?";
         this.hp = 0;
+        this.attackStat = 0;
         this.counterAttackStat = 0;
         this.characterImage = "?";
     },
+}
+var playerCharacter = character;
+var opponentCharacter = character;
+
+function rewrite() {
+    for(i=0; i < characterChoices.length; i++) {
+        console.log(characterChoices[i][0]);
+        $("#" + characterChoices[i][0]).empty();
+        $("#" + characterChoices[i][0]).removeElement("id");
+    }
 }
 
-var opponentCharacter = {
-    name: "?",
-    hp: 0,
-    counterAttackStat: 0,
-    characterImage: "?",
-    pick = function(arr) {
-        this.name = arr[0];
-        this.hp = arr[1];
-        this.counterAttackStat = arr[3];
-        this.characterImage = arr[4];
-    },
-    resetCharacter = function() {
-        this.name = "?";
-        this.hp = 0;
-        this.counterAttackStat = 0;
-        this.characterImage = "?";
-    },
-}
+$(document).ready(function() {
+
+    $("#Luke").on("click", function() {
+        if(playerCharacter.name === "?") {
+        playerCharacter.pickMe(characterChoices[0]);
+        rewrite();
+        }
+    });
+
+});
