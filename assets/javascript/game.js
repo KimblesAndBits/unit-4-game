f0bcd5d6d58dcbf8483ed4b5552a40788b8c2328
KimblesAndBits <wearethekimbles@gmail.com>
2019-05-02 21:20:48 -0700

    working enemy selector, mostly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,10 @@ var characterChoices = [
 ["Vader", 140, 6, 5, '<div class="card" alt="Vader" style="width: 100%;"><img src="assets/images/vader.jpeg" class="card-img-top" alt="Darth Vader"><div class="card-body"><h5 class="card-title">Darth Vader</h5><p class="card-text" id="Vader-text">140 HP</p></div></div>'],
 ["Boba-Fett", 125, 7, 16, '<div class="card" alt="Boba-Fett" style="width: 100%;"><img src="assets/images/fett.jpeg" class="card-img-top" alt="Boba Fett"><div class="card-body"><h5 class="card-title">Boba Fett</h5><p class="card-text" id="Boba-Fett-text">125 HP</p></div></div>']];
 
-var character = {
+var usedNames = [];
+var opponentsFought = 0;
+
+const character = {
     name: "?",
     hp: 0,
     attackStat: 0,
@@ -26,8 +29,9 @@ var character = {
     },
 }
 //make two variables that equal character objects
-var playerCharacter = character;
-var opponentCharacter = character;
+var playerCharacter = Object.create(character);
+var opponentCharacter = Object.create(character);
+
 //rewrite the info in the html after the player is chosen
 function playerPickRewrite() {
     //empty the player chooser divs
@@ -38,13 +42,21 @@ function playerPickRewrite() {
     $("#PC1").html(playerCharacter.characterCard);
     //change the "choose a player" text
     $("#player-head").text("Player!");
-    //initialize a variable to count through the opponent choice divs
-    var oppNum = 1;
-    //move through characterChoices and assign the card html to each column as long as it isn't the chosen character
+    rewriteOpponentSelector();
+}
+//empty the opponent selector area then
+//check each character against usedNames to make sure we don't display
+//the one's who have been defeated or the ones who are currently fighting
+function rewriteOpponentSelector() {
+    for(var oppNum=1; oppNum < 5; oppNum++) {
+        $(".opponent-" + oppNum).empty();
+    }
+    var opponentSelector = 1;
     for(var oppId=0; oppId < characterChoices.length; oppId++) {
-        if(playerCharacter.name !== characterChoices[oppId][0]){
-            $(".opponent-" + oppNum).html(characterChoices[oppId][4]);
-            oppNum++;
+        if(jQuery.inArray(characterChoices[oppId][0], usedNames) === -1){
+            $(".opponent-" + opponentSelector).html(characterChoices[oppId][4]);
+            $(".opponent-" + opponentSelector).attr("alt", characterChoices[oppId][0])
+            opponentSelector++;
         }
     }
 }
@@ -53,12 +65,14 @@ $(document).ready(function() {
     //pick a player character
     $(".player-character").on("click", function() {
         //use the attribute "alt" of the card we clicked to find out which character we picked
-        //this corresponds with the name of the characters in the array of possible characters
-        var chosenName = $(this).attr("alt");
+        //this corresponds with the name of the characters in the array of possible characters and add this to the usedNames array
+        var chosenName;
         //if the name in the alt attribute matches the first part of the index of the array, that is the player's choice
         //and use the pickMe method to assign the info to the playerCharacter object
         //then refresh the page
         if(playerCharacter.name === "?") {
+            chosenName = $(this).attr("alt");
+            usedNames[0] = chosenName;  
             for(var characterIdx = 0; characterIdx < characterChoices.length; characterIdx++){
                 if(chosenName === characterChoices[characterIdx][0]) {
                     playerCharacter.pickMe(characterChoices[characterIdx]);
@@ -68,6 +82,21 @@ $(document).ready(function() {
         }
     });
 
-    $(".")
+    $(".opponent-choice").on("click", function() {
+        var chosenOpponent;
+        if(opponentCharacter.name === "?") {
+            chosenOpponent = $(this).attr("alt");
+            opponentsFought++;
+            usedNames[opponentsFought] = chosenOpponent;
+            for(var characterIdx = 0; characterIdx < characterChoices.length; characterIdx++){
+                if(chosenOpponent === characterChoices[characterIdx][0]) {
+                    opponentCharacter.pickMe(characterChoices[characterIdx]);
+                    console.log(opponentCharacter);
+                    console.log(playerCharacter);
+                    rewriteOpponentSelector();
+                }
+            }
+        } 
+    })
 
 });
